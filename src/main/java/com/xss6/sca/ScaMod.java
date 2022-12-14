537649b67df7de1102a873a294871451d45f480b
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-12-14 13:34:31 -0700

    Update ScaMod.java

--- src/main/java/com/xss6/sca/ScaMod.java
+++ src/main/java/com/xss6/sca/ScaMod.java
@@ -4,6 +4,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import com.xss6.sca.event.EventProcessor;
 
 @Mod(modid = ScaMod.MODID, name = ScaMod.NAME, version = ScaMod.VERSION)
 public class ScaMod
@@ -12,7 +13,8 @@ public class ScaMod
     public static final String NAME = "SCA MOD";
     public static final String VERSION = "0.0.1";
 
-
+    public static EventProcessor EVENT_PROCESSOR;
+    
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
