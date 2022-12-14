fb303432f8afa487a26895cc5ba55068aa2b4eed
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-12-14 13:15:20 -0700

    Create PerspectiveEvent.java

---
+++ src/main/java/com/xss6/sca/event/events/PerspectiveEvent.java
@@ -0,0 +1,20 @@
+package com.xss6.sca.event.events;
+
+import com.xss6.sca.event.EventStage;
+
+
+public class PerspectiveEvent extends EventStage {
+    private float aspect;
+
+    public PerspectiveEvent(float aspect) {
+        this.aspect = aspect;
+    }
+
+    public float getAspect() {
+        return aspect;
+    }
+
+    public void setAspect(float aspect) {
+        this.aspect = aspect;
+    }
+}
