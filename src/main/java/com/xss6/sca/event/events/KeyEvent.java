32ed8bffc63fdc554ad7f0f1dc0c817a1c32803a
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-12-14 13:12:19 -0700

    Create KeyEvent.java

---
+++ src/main/java/com/xss6/sca/event/events/KeyEvent.java
@@ -0,0 +1,15 @@
+package com.xss6.sca.event.events;
+
+import com.xss6.sca.event.EventStage;
+
+public class KeyEvent
+        extends EventStage {
+    public boolean info;
+    public boolean pressed;
+
+    public KeyEvent(int stage, boolean info, boolean pressed) {
+        super(stage);
+        this.info = info;
+        this.pressed = pressed;
+    }
+}
