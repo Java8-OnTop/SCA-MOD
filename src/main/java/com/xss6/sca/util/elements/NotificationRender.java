0bfce5099496804ed6278721f9d7d7a2e0df86e4
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-12-15 09:22:33 -0700

    Create NotificationRender.java

---
+++ src/main/java/com/xss6/sca/util/elements/NotificationRender.java
@@ -0,0 +1,14 @@
+package com.xss6.sca.util.elements;
+
+public class NotificationRender {
+    public int x;
+    public int y;
+    public float animationVal;
+
+
+    public NotificationRender(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.animationVal = 0;
+    }
+}
