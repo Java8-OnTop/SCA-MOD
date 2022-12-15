2f726b425b2baa5ed0d9021320c2eadfa168b1d2
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-12-14 20:51:10 -0700

    Create Events.java

---
+++ src/main/java/com/xss6/sca/event/Events.java
@@ -0,0 +1,60 @@
+package com.xss6.sca.event;
+
+import com.google.common.base.Strings;
+
+import com.xss6.sca.event.events.*;
+import com.xss6.sca.util.*;
+import com.xss6.sca.ScaMod;
+
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.play.server.SPacketEntityStatus;
+import net.minecraft.network.play.server.SPacketPlayerListItem;
+import net.minecraft.network.play.server.SPacketPlayerPosLook;
+import net.minecraft.network.play.server.SPacketTimeUpdate;
+
+import net.minecraftforge.client.event.*;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+
+import org.jetbrains.annotations.NotNull;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Events implements Globals {
+    private long time = -1;
+
+    public Events() {
+        MinecraftForge.EVENT_BUS.register(this);
+        WurstplusThree.EVENT_PROCESSOR.addEventListener(this);
+    }
+
+    public void unload() {
+        MinecraftForge.EVENT_BUS.unregister(this);
+        WurstplusThree.EVENT_PROCESSOR.removeEventListener(this);
+    }
+  
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void onRenderGameOverlayEvent(RenderGameOverlayEvent.Text event) {
+        if (event.getType().equals(RenderGameOverlayEvent.ElementType.TEXT)) {
+            ScaledResolution resolution = new ScaledResolution(mc);
+            Render2DEvent render2DEvent = new Render2DEvent(event.getPartialTicks(), resolution);
+            GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+    }
+}
