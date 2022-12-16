6dfab0afc34edb5a1206a8ec2d1d65db13bc698a
XSS6 <72362725+Java8-OnTop@users.noreply.github.com>
2022-12-16 11:49:33 -0700

    Update ToggleMessages.java

--- src/main/java/com/xss6/sca/hack/hacks/chat/ToggleMessages.java
+++ src/main/java/com/xss6/sca/hack/hacks/chat/ToggleMessages.java
@@ -1,5 +1,25 @@
 package com.xss6.sca.hack.hacks.chat;
 
-public class ToggleMessages {
+import com.xss6.sca.hack.Hack;
+import com.xss6.sca.*;
 
+public class ToggleMessages extends Hack{
+  
+  double dDelayTimer = 0.1;
+  
+  public ToggleMessages() {
+    super(ToggleMessages());
+  }
+  
+  void onEnable() {
+    system.Out.println("ToggleMessages Enabled");
+  }
+  
+  void onToggle() {
+    if(Hack.getHackByName("Toggle Messages").isEnabled()) {
+      Hack.getHackByName("Toggle Messages").Disable();
+    }
+    else {
+    }
+  }
 }
